c065f881058d2caa34fe17f876215deda026da48
abcdmku <63693423+abcdmku@users.noreply.github.com>
2026-08-12 21:41:37 -0500

    build(contracts): add a public integration boundary

---
+++ packages/contracts/src/integration-unstable.ts
@@ -0,0 +1,35 @@
+import * as HttpApi from "effect/unstable/httpapi/HttpApi";
+import * as RpcGroup from "effect/unstable/rpc/RpcGroup";
+
+import {
+  EnvironmentAuthHttpApi,
+  EnvironmentMetadataHttpApi,
+  EnvironmentOrchestrationHttpApi,
+} from "./environmentHttp.ts";
+import { WsOrchestrationSubscribeShellRpc, WsOrchestrationSubscribeThreadRpc } from "./rpc.ts";
+
+export { EnvironmentAuthorizationError } from "./auth.ts";
+
+export {
+  EnvironmentAuthInvalidError,
+  EnvironmentHttpApi,
+  EnvironmentHttpCommonError,
+  EnvironmentScopeRequiredError,
+} from "./environmentHttp.ts";
+export {
+  ClientOrchestrationCommand,
+  ORCHESTRATION_WS_METHODS,
+  OrchestrationGetSnapshotError,
+  OrchestrationSubscribeShellInput,
+  OrchestrationSubscribeThreadInput,
+} from "./orchestration.ts";
+
+export const IntegrationEnvironmentHttpApi = HttpApi.make("environment")
+  .add(EnvironmentMetadataHttpApi)
+  .add(EnvironmentAuthHttpApi)
+  .add(EnvironmentOrchestrationHttpApi);
+
+export const IntegrationWsRpcGroup = RpcGroup.make(
+  WsOrchestrationSubscribeShellRpc,
+  WsOrchestrationSubscribeThreadRpc,
+);
